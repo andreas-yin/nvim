b687094788b5792b72c01dc3eb96e725a555fb12
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2024-03-26 01:39:45 +0100

    remove eslint_d, already installed

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
         require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.black,
 			},
 		})
