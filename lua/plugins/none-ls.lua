5d1755f2026c0bd3aea2c0801659170858a1f79f
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2024-03-27 11:25:39 +0100

    add format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,14 +3,27 @@ return {
   dependencies = {"nvimtools/none-ls-extras.nvim"},
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
         require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.black,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = true })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gi", vim.lsp.buf.format, {})
