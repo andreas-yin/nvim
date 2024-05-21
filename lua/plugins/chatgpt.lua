a28ad33c60b5f023c67914ea12cdaa650e25d779
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2024-05-21 23:09:26 +0200

    change max_tokens and model in chatgpt plugin

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -156,10 +156,10 @@ return {
         },
       },
       openai_params = {
-        model = "gpt-4-turbo-preview",
+        model = "gpt-4o",
         frequency_penalty = 0,
         presence_penalty = 0,
-        max_tokens = 300,
+        max_tokens = 4096,
         temperature = 0,
         top_p = 1,
         n = 1,
