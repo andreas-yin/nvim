30568c3a7f43b5d51cd783e3ae426b0ef0616504
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2024-06-28 21:42:44 +0200

    add pylint and isort

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,18 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = {"nvimtools/none-ls-extras.nvim"},
+	dependencies = { "nvimtools/none-ls-extras.nvim" },
 	config = function()
 		local null_ls = require("null-ls")
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
-        require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.diagnostics.pylint,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
